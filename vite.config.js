dfa2f260a6fb592a2e0fae96b3b1b9740d4eb8a9
MaximilianoFerro <90514671+MaximilianoFerro@users.noreply.github.com>
2026-05-11 09:30:05 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Nombre exacto de tu repositorio en GitHub
+const REPO_NAME = '/school-management-platform/'
+
 export default defineConfig({
   plugins: [react()],
+  base: REPO_NAME,
 })
